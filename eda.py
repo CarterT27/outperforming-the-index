d5be71c7bcd4b599f6da90e753923d91e3bc58e8
Carter Tran <carter.tran@gmail.com>
2025-05-19 17:02:20 -0700

    add data

--- eda.py
+++ eda.py
@@ -4,19 +4,21 @@ from kagglehub import KaggleDatasetAdapter
 import os
 import pandas as pd
 # %%
-def load_data():
-    file_path = "sp500_stocks.csv"
+def load_data(file_path: str = "sp500_stocks.csv"):
     if not os.path.exists(file_path):
         df = kagglehub.dataset_load(
             KaggleDatasetAdapter.PANDAS,
             "andrewmvd/sp-500-stocks",
             file_path,
-            pandas_kwargs={"parse_dates": True, "index_col": "Date"},
-        ).dropna(subset=['Adj Close'])
+            pandas_kwargs={"parse_dates": True},
+        )
+        if file_path == 'sp500_stocks.csv':
+            df = df.dropna(subset=['Adj Close']).set_index('Date')
         df.to_parquet(file_path.replace('.csv', '.parquet'))
     else:
         df = pd.read_parquet(file_path)
     return df
 # %%
-df = load_data()
-df.head()
+for filepath in ['sp500_stocks.csv', 'sp500_index.csv', 'sp500_companies.csv']:
+    df = load_data(filepath)
+    print(df.head())
