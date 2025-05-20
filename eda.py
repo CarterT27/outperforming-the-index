2294982b540b4e2cce82933b2b545b12ac2d558b
Carter Tran <carter.tran@gmail.com>
2025-05-19 17:06:36 -0700

    add plot ideas

--- eda.py
+++ eda.py
@@ -22,3 +22,21 @@ def load_data(file_path: str = "sp500_stocks.csv"):
 for filepath in ['sp500_stocks.csv', 'sp500_index.csv', 'sp500_companies.csv']:
     df = load_data(filepath)
     print(df.head())
+# %%
+# 1. Line plot of S&P 500 Index
+pass
+# %%
+# 2. Treemap of S&P at a given point in time
+pass
+# %%
+# 3. Area plot of S&P 500 Index
+pass
+# %%
+# 4. Bar chart of daily, weekly, and monthly returns of S&P 500 Constituents
+pass
+# %%
+# 5. Bar chart of daily, weekly, and monthly returns of S&P 500 Index
+pass
+# %%
+# 6. Scatter plot of daily returns of S&P 500 Constituents over time
+pass
